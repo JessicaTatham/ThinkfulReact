8abf66c1dfc0db991e319d36787d7ef29f258cf5
JessicaTatham <jectatham@gmail.com>
2019-01-09 11:22:54 -0800

    Add props

--- js/components/person-list.js
+++ js/components/person-list.js
@@ -4,13 +4,12 @@ var Person = require('./person');
 
 var PersonList = React.createClass({
     render: function() {
-        var people = [];
-        for (var i=0; i<5; i++) {
-            people.push(<Person />);
-        }
+        var name = 'Derek Zoolander';
+        var imageUrl = 'https://s3.drafthouse.com/images/made/zoolander-blue-steel-ben-stiller-today-tease-150720_690101f3edc8f1aa9fc8b8b754527bd5_800_450_81_s.jpg';
+        var job = 'Male model';
         return (
             <div className="person-list">
-                {people}
+                <Person name={name} imageUrl={imageUrl} job={job} />
             </div>
         );
     }
